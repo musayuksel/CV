810894484130ef962f3be6c6c8b8088a6fbb5984
musayuksel <musayuxel@gmail.com>
2022-02-27 22:11:11 +0000

    Feat:Add navBar into the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "../styles/header.css";
+import NavBar from "./NavBar";
 
 export default function Header() {
   return (
     <header id="home">
+      <NavBar />
       <h1>Musa Yuksel</h1>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni,
