eb47dfcb9d3adc5fe517403605ec0c76306709b6
musayuksel <musayuxel@gmail.com>
2022-02-27 15:18:14 +0000

    Add classnames for skills component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 export default function Skills({ skills }) {
-  const skillsList = skills.map((skill, index) => <li key={index}>{skill}</li>);
-  return <ul>{skillsList}</ul>;
+  const skillsList = skills.map((skill, index) => (
+    <li key={index} className="skills">
+      {skill}
+    </li>
+  ));
+  return <ul className="skills-container">{skillsList}</ul>;
 }
