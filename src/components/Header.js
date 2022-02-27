12815b7aeb378d966702d3eae982ba79b2f6f9b2
musayuksel <musayuxel@gmail.com>
2022-02-27 16:27:56 +0000

    Add header classnames

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,27 +6,37 @@ export default function Header() {
   return (
     <header>
       <h1>Musa Yuksel</h1>
-      <h2>Full Stack Web Developer</h2>
-      <MdAlternateEmail />
-      <a
-        target={"blank"}
-        href="mailto:musayuxel@gmail.com?subject=About%20job%20application"
-      >
-        musayuxel@gmail.com
-      </a>
-      <MdOutlineLocationOn />
-      <p>United Kingdom</p>
-      <BsLinkedin />
-      <a
-        href="https://www.linkedin.com/in/musa-yuksel-625838205/"
-        target={"blank"}
-      >
-        musa-yuksel
-      </a>
-      <BsGithub />
-      <a href="https://github.com/musayuksel" target={"blank"}>
-        musa-yuksel
-      </a>
+      <p>Full Stack Web Developer</p>
+      <section className="contact-infos">
+        <div>
+          <MdAlternateEmail />
+          <a
+            target={"blank"}
+            href="mailto:musayuxel@gmail.com?subject=About%20job%20application"
+          >
+            musayuxel@gmail.com
+          </a>
+        </div>
+        <div>
+          <MdOutlineLocationOn />
+          <p>United Kingdom</p>
+        </div>
+        <div>
+          <BsLinkedin />
+          <a
+            href="https://www.linkedin.com/in/musa-yuksel-625838205/"
+            target={"blank"}
+          >
+            musa-yuksel
+          </a>
+        </div>
+        <div>
+          <BsGithub />
+          <a href="https://github.com/musayuksel" target={"blank"}>
+            musa-yuksel
+          </a>
+        </div>
+      </section>
     </header>
   );
 }
