d917a84c6a648859607661727d06adc948e58cbd
musayuksel <musayuxel@gmail.com>
2022-02-27 19:12:51 +0000

    feat:Add basic header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/header.css";
+
+export default function Header() {
+  return (
+    <header id="home">
+      <h1>Musa Yuksel</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni,
+        tenetur.
+      </p>
+    </header>
+  );
+}
