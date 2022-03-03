7fcb4093a64f438ced6f656e28efa46e7e41c544
musayuksel <musayuxel@gmail.com>
2022-03-03 13:05:27 +0000

    feat: Add state for scroll event

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/navbar.css";
 import { MdOutlineMenu, MdClose } from "react-icons/md";
 export default function NavBar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  // const [offset, setOffset] = useState(0);
+  const [scrolled, setScrolled] = useState();
+  // const classes = classNames("header", {
+  //   scrolled: scrolled,
+  // });
+  useEffect((_) => {
+    const handleScroll = (_) => {
+      if (window.pageYOffset > 1) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return (_) => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  // console.log(offset);
   return (
-    <nav className={`${isMenuOpen ? "open" : ""}`}>
+    <nav
+      className={`${isMenuOpen ? "open" : ""} ${scrolled ? "scrolled" : ""}`}
+    >
       <div
         className={`menu ${isMenuOpen ? "cross" : ""}`}
         onClick={() => setIsMenuOpen((prev) => !prev)}
