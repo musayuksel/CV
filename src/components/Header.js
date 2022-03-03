e3358eba0d1abb76c441319f4ebcead5a553ffb3
musayuksel <musayuxel@gmail.com>
2022-03-03 13:05:12 +0000

    feat: Add download cv button

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,15 @@ export default function Header() {
   return (
     <header id="home">
       <NavBar />
-      <img src="./musa.png" alt="picture of musa" />
       <h1>Musa Yuksel</h1>
       <p>Full Stack Web Devoloper</p>
+      <a
+        className="download-cv"
+        href="https://drive.google.com/file/d/1oQ-KyjEeJ_wayq8xyo-2gYsRiqN7zfWZ/view?usp=sharing"
+        target={"_blank"}
+      >
+        Download CV
+      </a>
     </header>
   );
 }
