b9b1bc187faeb23edd1970e1ea29cc4671a4d1d6
musayuksel <musayuxel@gmail.com>
2022-03-10 11:53:40 +0000

    Refactor:Update CV lik

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ export default function Header() {
       <p>Full Stack Web Devoloper</p>
       <a
         className="download-cv"
-        href="https://drive.google.com/file/d/1T3Kff9q5IGl1SHwTc7YK2uNe71poy6io/view?usp=sharing"
+        href="https://drive.google.com/file/d/1MoSVjXf714ATZR-vaT1e8OwxUjxeAZPr/view?usp=sharing"
         target={"_blank"}
       >
         Download CV
